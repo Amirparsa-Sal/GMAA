b8096c529b86123f66f3876d5d50c518ab7a37de
Amirparsa Salmankhah <60196448+Amirparsa-Sal@users.noreply.github.com>
2023-08-26 15:39:26 +0330

    remove mapping from celeba-hq to celeba

--- data/CelebAHQ/process.py
+++ data/CelebAHQ/process.py
@@ -16,19 +16,9 @@ import argparse
 from matplotlib import pyplot as plt
 
 
-src_hq_path = '/beegfs/home/huyx/ADV/data/test-data/CelebAMask-HQ/CelebA-HQ-img'
+src_hq_path = '/content/data_faces/img_align_celeba'
 dest_hq_path = 'data/CelebAHQ/CelebA-HQ-filter-img/images_aligned'
-map_path = 'data/CelebAHQ/CelebA-HQ-to-CelebA-mapping.txt'
 au_path = 'data/CelebAHQ/list_attr_HQ-filter-img.txt'
-
-map_dict = {}
-with open(map_path, "r") as f:
-    lines = f.readlines()
-    lines = lines[1:]
-    for line in lines:
-        line = line.strip()
-        split = line.split()
-        map_dict[split[0]] = 'frame_det_00_' + split[1].zfill(6)
 
 valid_name_list = []
 with open(au_path, "r") as f:
@@ -41,10 +31,9 @@ with open(au_path, "r") as f:
 
 ct = 0
 for src_name in os.listdir(src_hq_path):
-    dest_name = map_dict[src_name]
-    if dest_name in valid_name_list:
+    if src_name in valid_name_list:
         src_path = os.path.join(src_hq_path, src_name)
-        dest_path = os.path.join(dest_hq_path, dest_name)
+        dest_path = os.path.join(dest_hq_path, 'frame_det_00_' + src_name)
 
         shutil.copy(src_path, dest_path)
         ct += 1
